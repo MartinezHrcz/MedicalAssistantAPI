2e48908e6dfc9d19f22af0de191c77e5752c3b48
Herczeg Martinez <160034549+MartinezHrcz@users.noreply.github.com>
2025-10-06 22:03:55 +0200

    feat(db): make db migration

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,7 +1,16 @@
+using Api.DB;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<MedicalDataContext>(
+    options =>
+    {
+       options.UseSqlite(builder.Configuration.GetConnectionString("SQLite")); 
+    });
 
 var app = builder.Build();
 
