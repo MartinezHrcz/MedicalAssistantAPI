81482a6edfd905e59d538ecc9702d945f38cd177
Herczeg Martinez <160034549+MartinezHrcz@users.noreply.github.com>
2025-10-06 21:17:15 +0200

    feat: created Patient model with input validation (required, and regex for Taj number )

---
+++ Api/Models/Patient.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Api.Models;
+
+public class Patient
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+    
+    [Required]
+    [MaxLength(50)]
+    public string Name { get; set; }
+    
+    public string Address { get; set; }
+
+    [Required]
+    [RegularExpression(@"^\d{3}-\d{3}-\d{3}$")]
+    public string Taj { get; set; }
+
+    public string Complaints { get; set; }
+    
+    public DateTime timeOfAdmission { get; set; } =  DateTime.Now;
+    
+}
