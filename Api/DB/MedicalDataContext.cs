117ce198707ff4b05f14628115f71d429bed4764
Herczeg Martinez <160034549+MartinezHrcz@users.noreply.github.com>
2025-10-06 21:51:36 +0200

    feat(db): create datacontext

---
+++ Api/DB/MedicalDataContext.cs
@@ -0,0 +1,12 @@
+﻿using Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.DB;
+
+public class MedicalDataContext: DbContext
+{
+    public MedicalDataContext(DbContextOptions options) : base(options)
+    { }
+
+    public virtual DbSet<Patient> Patients { get; set; }
+}
