b42466bf67b8c6cf37af8a74f3f98d674edb7ac5
Herczeg Martinez <160034549+MartinezHrcz@users.noreply.github.com>
2025-10-07 16:28:23 +0200

    feat: add PatientService interface

---
+++ Api/Services/IPatientService.cs
@@ -0,0 +1,15 @@
+﻿using Api.DTOs;
+using Api.Models;
+
+namespace Api.Services;
+
+public interface IPatientService
+{
+    Task<IEnumerable<Patient>> GetPatientsAsync();
+    Task<PatientDto> GetPatientByIdAsync(int id);
+    Task<IEnumerable<PatientDto>> GetPatientByNameAsync(string name);
+    Task<PatientDto> GetPatientByTajAsync(string taj);
+    Task<PatientDto> CreatePatientAsync(CreatePatientDto dto);
+    Task<PatientDto> UpdatePatientAsync(UpdatePatientDto dto);
+    Task DeletePatient(int id);
+}
