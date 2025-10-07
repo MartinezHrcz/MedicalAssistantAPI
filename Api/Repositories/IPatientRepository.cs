d19733d3de95b6dcc085d60b3e34b8209c4cb2bf
Herczeg Martinez <160034549+MartinezHrcz@users.noreply.github.com>
2025-10-07 15:27:07 +0200

    feat(repo): add interface for PatientRepository

---
+++ Api/Repositories/IPatientRepository.cs
@@ -0,0 +1,14 @@
+﻿using Api.Models;
+
+namespace Api.Repositories;
+
+public interface IPatientRepository
+{
+    Task<IEnumerable<Patient>> GetAllPatients();
+    Task<Patient> GetPatientById(string id);
+    Task<Patient> GetPatientByName(string name);
+    Task<Patient> GetPatientByTaj(string taj);
+    Task<Patient> CreatePatient(Patient patient);
+    Task<Patient> UpdatePatient(Patient patient);
+    Task DeletePatient(string id);
+}
