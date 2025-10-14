f34233bcdc0e216fdb522305cae56b57219ebfd2
Herczeg Martinez <160034549+MartinezHrcz@users.noreply.github.com>
2025-10-14 18:25:02 +0200

    feat: add doctor repository interface

---
+++ Api/Repositories/IDoctorRepository.cs
@@ -0,0 +1,14 @@
+﻿using Api.Shared.Models;
+
+namespace Api.Repositories;
+
+public interface IDoctorRepository
+{
+    Task<IEnumerable<Doctor>> GetAllDoctors();
+    Task<Doctor> GetDoctorById(int id);
+    Task<IEnumerable<Doctor>> GetDoctorsByName(string name);
+    Task<Doctor> CreateDoctor(Doctor doctor);
+    Task<Doctor> UpdateDoctor(Doctor doctor);
+    Task DeleteDoctor(int id);
+    Task<bool> DoctorEmailExist(string email);
+}
