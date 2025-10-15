aaa1e6a662979b5e37dc32369845bc86b5119df9
Herczeg Martinez <160034549+MartinezHrcz@users.noreply.github.com>
2025-10-15 13:20:00 +0200

    feat: get all and by id implemented

--- Api/Services/DoctorService.cs
+++ Api/Services/DoctorService.cs
@@ -1,6 +1,7 @@
 ﻿using Api.Repositories;
 using Api.Shared.Models;
 using Api.Shared.Models.DTOs;
+using Api.Mappers;
 using Microsoft.AspNetCore.Identity;
 
 namespace Api.Services;
@@ -20,11 +21,13 @@ public class DoctorService : IDoctorService
     public async Task<IEnumerable<DoctorDto>> GetDoctorsAsync()
     {
         IEnumerable<Doctor> doctors = await _doctorRepository.GetAllDoctors();
+        return DoctorMapper.ToDTO(doctors);
     }
 
     public async Task<DoctorDto?> GetDoctorAsync(int id)
     {
-        throw new NotImplementedException();
+        Doctor doctor = await _doctorRepository.GetDoctorById(id) ??  throw new KeyNotFoundException();
+        return DoctorMapper.ToDTO(doctor);
     }
 
     public async Task<DoctorDto> CreateDoctorAsync(RegisterDoctorDto doctor)
