bac8e854073a64e7cc98a98778e52b4ff1377765
Herczeg Martinez <160034549+MartinezHrcz@users.noreply.github.com>
2025-10-15 13:18:03 +0200

    feat: doctor mapper made

---
+++ Api/Services/DoctorService.cs
@@ -0,0 +1,64 @@
+﻿using Api.Repositories;
+using Api.Shared.Models;
+using Api.Shared.Models.DTOs;
+using Microsoft.AspNetCore.Identity;
+
+namespace Api.Services;
+
+public class DoctorService : IDoctorService
+{
+    
+    private readonly IDoctorRepository  _doctorRepository;
+    private PasswordHasher<DoctorDto> _passwordHasher;
+
+    public DoctorService(IDoctorRepository doctorRepository)
+    {
+        _doctorRepository = doctorRepository;
+        _passwordHasher = new PasswordHasher<DoctorDto>();
+    }
+
+    public async Task<IEnumerable<DoctorDto>> GetDoctorsAsync()
+    {
+        IEnumerable<Doctor> doctors = await _doctorRepository.GetAllDoctors();
+    }
+
+    public async Task<DoctorDto?> GetDoctorAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<DoctorDto> CreateDoctorAsync(RegisterDoctorDto doctor)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<DoctorDto> UpdateDoctorAsync(UpdateDoctorDto doctor)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> DeleteDoctorAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IEnumerable<PatientDto>> GetPatientsOfDoctor(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> AddPatientAsync(int doctorId, int patientId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> RemovePatientAsync(int doctorId, int patientId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<DoctorDto> LoginDoctorAsync(LoginDoctorDto doctor)
+    {
+        throw new NotImplementedException();
+    }
+}
